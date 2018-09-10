3c19d500203c79065a1e236aa5406618381f970b
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-10 22:59:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 function preventRefreshOnSubmit(){
     document.querySelector('form').addEventListener('submit', function(event){
+
       event.preventDefault()
     })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let li = document.createElement("li")
+  li.innerHTML = `${retrieveEmployeeInformation()}`
+  return document.querySelector('.employee-list').append(li)
+}
+
+function addNewLiOnClick() {
+  document.querySelector('form').addEventListener('click', function(e){
+    e.preventDefault()
+     addNewElementAsLi()
+     document.querySelector('input').value = ''
+  })
+
+}
+
+function clearEmployeeListOnLinkClick() {
+  document.querySelector('a').addEventListener('click', function(e){
+    e.preventDefault()
+    document.querySelector('.employee-list').innerHTML = ''
+  })
+}
